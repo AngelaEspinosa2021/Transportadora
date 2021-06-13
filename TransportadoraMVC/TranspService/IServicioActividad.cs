0e7b064599bafa2647a16ff666d11941159a5619
jccarden23 <jimeingsistemas@gmail.com>
2021-06-13 16:04:25 -0500

    Creacion servicio actividad

---
+++ TransportadoraMVC/TranspService/IServicioActividad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TranspService.Modelos;
+
+namespace TranspService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioActividad" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioActividad
+    {
+        [OperationContract]
+        List<Actividad> ListarActivida();
+
+        [OperationContract]
+        void AgregarActividad(Actividad act);
+
+        [OperationContract]
+        void EditarActividad(long Id, Actividad act);
+
+        [OperationContract]
+        void EliminarActividad(long Id);
+
+        [OperationContract]
+        Actividad BuscarActividad(long Id);
+    }
+}
---
+++ TransportadoraMVC/TranspService/ServicioActividad.svc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TranspService.Modelos;
+
+namespace TranspService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioActividad" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioActividad.svc o ServicioActividad.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServicioActividad : IServicioActividad
+    {
+        transportadoraEntities bd = new transportadoraEntities();
+        public void AgregarActividad(Actividad act)
+        {
+            bd.Actividad.Add(act);
+            bd.SaveChanges();
+        }
+
+        public Actividad BuscarActividad(long Id)
+        {
+            var buscar = bd.Actividad.Find(Id);
+            return buscar;
+        }
+
+        public void DoWork()
+        {
+        }
+
+        public void EditarActividad(long Id, Actividad act)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EliminarActividad(long Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Actividad> ListarActivida()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
